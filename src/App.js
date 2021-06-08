9266d0801e41b5b9b099c05ad73582096e981ed9
rabeb jaouani <jaouanirabeb@gmail.com>
2021-06-08 02:43:42 +0100

    checkpoint props

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  render() {
+    state={
+      Person ={ fullName="rabeb jaouani", 
+      imgSrc="/imageInPublic", 
+      profession="informaticienne"
+    },
+      shows:true
+    }
+    return (
+      <div>
+        
+      </div>
+    )
+  }
 }
-
-export default App;
